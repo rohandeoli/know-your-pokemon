2b9c2f6b261d3ebcb1e4b452b46310b438fb82b8
rohandeoli <rohan.deoli98@gmail.com>
2025-06-26 21:22:08 +0530

    Enhance `PokemonCard` with detailed Pokémon information display
    - Updated `PokemonCard` to include Pokémon name, ID, image, types, height, and weight.
    - Integrated reusable `Badge`, `CardTitle`, and `CardDescription` components for better styling.
    - Adjusted layout proportions for improved responsiveness and content distribution.

--- src/components/pokemon-card/pokemon-card.tsx
+++ src/components/pokemon-card/pokemon-card.tsx
@@ -1,16 +1,35 @@
-import {Card, CardContent} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {Badge} from "@/components/ui/badge.tsx";
 
 export default function PokemonCard({data}: any) {
     return (
         <>
             <Card className="w-[95%] mx-auto mt-10">
                 <CardContent className="flex flex-row flex-wrap gap-2">
-                    <Card className="w-[40%]">
+                    <Card className="w-[30%]">
+                        <CardHeader>
+                            <CardTitle>{data?.name}</CardTitle>
+                            <CardDescription>
+                                #{data?.id}
+                            </CardDescription>
+                        </CardHeader>
                         <CardContent>
-                            <h1>Pokemon Card</h1>
+                            <img src={data?.sprites?.other.dream_world.front_default} alt={data?.name}/>
+                            <div className={"mt-8"}>
+                                {data?.types?.map((type: any, index: number) => {
+                                    return <Badge variant={index == 0 ? 'default' : 'outline'}
+                                                  className="mr-2">{type.type.name}</Badge>
+                                })}
+                            </div>
+                            <div className="mt-8">
+                                <em className={"font-bold"}>Height:</em> {data?.height / 10}m <br/>
+                            </div>
+                            <div className="mt-2">
+                                <em className={"font-bold"}>Weight:</em> {data?.weight / 10}kg <br/>
+                            </div>
                         </CardContent>
                     </Card>
-                    <Card className="w-[59%]">
+                    <Card className="w-[69%]">
                         <CardContent>
                             <h1>Pokemon Card 2</h1>
                         </CardContent>
